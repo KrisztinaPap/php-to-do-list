773e3c1238cb51aebb191f126c9af01da6b1abad
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-02 22:52:25 -0600

    Adds headers and new to-do form;

--- index.php
+++ index.php
@@ -12,6 +12,25 @@
 
 <h1>Welcome to Krisztina's PHP To-Do List</h1>
 
+<h2>Add a To-Do</h2>
+
+<form method="POST" action="index.php">
+    <label for="newTask">
+        Enter a new task:
+        <input 
+            id="newTask" 
+            name="newTask"
+            type="string"
+            value="">
+    </label>
+    <input type="submit" value="Add To List">
+</form>
+
+<h2>Active To-Dos</h2>
+
+<h2>Completed To-Dos</h2>
+
+<h2>Debugging</h2>
 
 
 <?php
