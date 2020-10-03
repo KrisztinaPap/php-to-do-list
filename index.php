456c7181b443a20eb7f8372e42f6918eb552db5b
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-02 22:43:26 -0600

    Adds session and initializes active and completed arrays;

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
+    // Starts session - so cookies can hold session data
+    session_start();
+    // If activeList array has not been initialized (new session), initialize both active and completed arrays
+    if ( !isset( $_SESSION['activeList'])){
+        $_SESSION['activeList'] = array();
+        $_SESSION['completedList'] = array();
+    }
+
     include './templates/header.php';
 ?>
 
 <h1>Welcome to Krisztina's PHP To-Do List</h1>
+
+
 
 <?php
     include './templates/footer.php';
