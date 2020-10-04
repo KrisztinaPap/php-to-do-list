5d347ea857431a31d577085c6fe40fdddc14eadd
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-04 11:55:16 -0600

    Adds tyling; Adds media query for desktop size;

--- index.php
+++ index.php
@@ -39,56 +39,63 @@
     }
 ?>
 
-<h1>Welcome to Krisztina's PHP To-Do List</h1>
+<h1>Krisztina's PHP To-Do List</h1>
 
-<h2>Add a To-Do</h2>
+<section id="addToDoSection">
+    <h2>Add a To-Do</h2>
+    <form method="POST" action="index.php">
+        <label for="newTask">
+            Enter a new task:
+            <input 
+                id="newTask" 
+                name="newTask"
+                type="string"
+                value="">
+        </label>
+        <button type="btn" name="add" value="Add To List"><i class="far fa-plus-square"></i></button>
+        <button type="btn" name="reset" value="Reset List"><i class="fas fa-power-off"></i></button>
+    </form>`
+</section>
 
-<form method="POST" action="index.php">
-    <label for="newTask">
-        Enter a new task:
-        <input 
-            id="newTask" 
-            name="newTask"
-            type="string"
-            value="">
-    </label>
-    <input type="submit" name="add" value="Add To List">
-    <input type="submit" name="reset" value="Reset List">
-</form>
-
-<h2>Active To-Dos</h2>
-<ul>
-<?php if ( !empty($_SESSION['activeList']) ) : ?> 
-    <!-- Received advice from Lindsey Graham re: adding id to following code line -->
-    <?php foreach ( $_SESSION[ 'activeList' ] as $id=>$task ) : ?>
-        <li>
-            <?php echo $task ?>
-            <form method="POST" action="index.php">
-                <!-- Received advice from Lindsey Graham re: how to add id number to following code line (htmlspecialchars) -->
-                <button id="delete-<?php echo $task?>" type="btn" name="deleteTask[<?php echo htmlspecialchars($id); ?>]" value="delete"><i class="fas fa-trash-alt"></i></button>
-                <button id="complete-<?php echo $task?>" type="btn" name="completeTask[<?php echo htmlspecialchars($id); ?>]" value="isCompleted"><i class="far fa-check-square"></i></button>
-            </form>
-        </li>
-    <?php endforeach; ?>
-<?php endif ?>
-
-
-</ul>
-
-<h2>Completed To-Dos</h2>
-
-<?php if ( !empty($_SESSION['completedList']) ) : ?> 
-        <?php foreach ( $_SESSION[ 'completedList' ] as $id=>$task ) : ?>
-            <?php if ( $task !== '') : ?>
+<section id="activeToDoSection">
+    <h2>Active To-Dos</h2>
+    <ul>
+        <?php if ( !empty($_SESSION['activeList']) ) : ?> 
+            <!-- Received advice from Lindsey Graham re: adding id to following code line -->
+            <?php foreach ( $_SESSION[ 'activeList' ] as $id=>$task ) : ?>
                 <li>
-                    <?php echo $task ?>
+                    <form method="POST" action="index.php">
+                        <div class="flex-container">
+                            <div class="task-name">
+                                <?php echo $task ?>
+                            </div>
+                            <div class="buttons">
+                                <!-- Received advice from Lindsey Graham re: how to add id number to following code line (htmlspecialchars) -->
+                                <button id="delete-<?php echo $task?>" type="btn" name="deleteTask[<?php echo htmlspecialchars($id); ?>]" value="delete"><i class="fas fa-trash-alt"></i></button>
+                                <button id="complete-<?php echo $task?>" type="btn" name="completeTask[<?php echo htmlspecialchars($id); ?>]" value="isCompleted"><i class="far fa-check-square"></i></button>
+                            </div>
+                        </div>
+                    </form>
                 </li>
-            <?php endif ?>
-        <?php endforeach; ?>
-<?php endif ?>
+            <?php endforeach; ?>
+        <?php endif ?>
+    </ul>
+</section>
 
-<h2>Debugging</h2>
-
+<section id="completedToDoSection">
+    <h2>Completed To-Dos</h2>
+    <ul>
+        <?php if ( !empty($_SESSION['completedList']) ) : ?> 
+                <?php foreach ( $_SESSION[ 'completedList' ] as $id=>$task ) : ?>
+                    <?php if ( $task !== '') : ?>
+                        <li>
+                            <?php echo $task ?>
+                        </li>
+                    <?php endif ?>
+                <?php endforeach; ?>
+        <?php endif ?>
+    </ul>
+</section>
 
 <?php
     include './templates/footer.php';
